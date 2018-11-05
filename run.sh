7e474d88a9d4fe11157e9a2256880bc9e0484ad7
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2018-11-05 11:16:00 +0900

    Update

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 export CI_NAME="WERCKER"
 export CI_BUILD_URL=$WERCKER_BUILD_URL
 
-export CI_BRANCH=$WERCKER_GIT_BRANCH
 GIT_BRANCH=$WERCKER_GIT_BRANCH
 if [ "$GIT_BRANCH" = "HEAD" ]; then
   GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 fi
 echo "Git branch: $GIT_BRANCH"
+export CI_BRANCH
